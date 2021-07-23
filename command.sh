8775414a7ce6e3306da3eb86c12887f11a0fce88
unknown <kweyz@daum.net>
2021-07-23 09:35:08 +0900

    dockerCommand

---
+++ command.sh
@@ -0,0 +1,25 @@
+# docker bulid
+docker build -t dockerid/repository:tag
+
+# docker images 
+docker images
+
+# docker login
+docker login
+
+# docker push
+docker push dockerid/repository:tagname
+
+# docker container list --> docker desktop checking
+
+# docker run
+docker run -d -p 8080:8080 dockerid/repository:tagname
+
+# docker ps
+docker ps
+
+# docker logs 
+docker logs
+
+# docker stop
+docker stop containerID
